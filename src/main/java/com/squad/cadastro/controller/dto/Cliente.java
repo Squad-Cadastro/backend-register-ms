a57670afbd6fd1bb54adc43ae7b297e029949a16
Guilherme Nogueira <guilherme.nogueiras@outlook.com>
2022-09-13 12:11:55 -0300

    exemplo controller

---
+++ src/main/java/com/squad/cadastro/controller/dto/Cliente.java
@@ -0,0 +1,39 @@
+package com.squad.cadastro.controller.dto;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
+    private String id;
+    private String nome;
+    private String email;
+
+    public Cliente(String id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
