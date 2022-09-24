cb7560921873872f97efec40b0a95538bca41fe1
Allan Caires <allancaires@icloud.com>
2022-09-24 18:58:53 -0300

    cep

--- src/main/java/com/squad/cadastro/controller/CadastroController.java
+++ src/main/java/com/squad/cadastro/controller/CadastroController.java
@@ -3,6 +3,7 @@ package com.squad.cadastro.controller;
 import com.squad.cadastro.controller.dto.Cliente;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import com.squad.cadastro.controller.EnderecoResponse;
 
 import java.util.UUID;
 
@@ -11,7 +12,7 @@ import java.util.UUID;
 @CrossOrigin("https://frontend-register-app-squad-cadastro.vercel.app/")
 public class CadastroController {
 
-    @GetMapping(value = "/cep/{cep}")
+    @GetMapping("/cep/{cep}")
     public EnderecoResponse buscarEndereco(@PathVariable String cep){
         return new RestTemplate().getForEntity("https://viacep.com.br/ws/"+ cep +"/json/", EnderecoResponse.class).getBody();
     }
--- src/main/java/com/squad/cadastro/controller/EnderecoResponse.java
+++ src/main/java/com/squad/cadastro/controller/EnderecoResponse.java
@@ -20,42 +20,54 @@ public class EnderecoResponse implements Serializable{
         this.complemento = complemento;
     }
 
-    public String getCep(String cep){
+    public String getCep(){
         return cep;
     }
-    public void setCep(String cep){
+    public void setCep(){
         this.cep = cep;
     }
-    public String getLogradouro(String logradouro){
+    public String getLogradouro(){
+
         return logradouro;
+
     }
     public void setLogradouro(String logradouro){
         this.logradouro = logradouro;
     }
-    public String getComplemento(String complemento){
+
+    public String getComplemento(){
         return complemento;
+
     }
     public void setComplemento(String complemento)
     {
         this.complemento = complemento;
     }
-    public String getBairro(String bairro){
+
+    public String getBairro(){
+
         return bairro;
+
     }
     public void setBairro(String bairro){
         this.bairro = bairro;
     }
-    public String getLocalidade(String localidade){
+
+    public String getLocalidade(){
+
         return localidade;
+
     }
     public void setLocalidade(String localidade){
         this.localidade = localidade;
     }
-    public String getUf(String uf){
+
+    public String getUf(){
+
         return uf;
+
     }
     public void setUf(String uf){
         this.uf = uf;
     }
-
 }
