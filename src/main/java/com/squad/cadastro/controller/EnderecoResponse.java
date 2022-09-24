18623f7831da9435097543f2c71fa417216c8a28
Allan Caires <allancaires@icloud.com>
2022-09-24 12:39:54 -0300

    cep

--- src/main/java/com/squad/cadastro/controller/EnderecoResponse.java
+++ src/main/java/com/squad/cadastro/controller/EnderecoResponse.java
@@ -1,7 +1,8 @@
 package com.squad.cadastro.controller;
-import serializable;
+import java.io.Serializable;
 
-public class ControllerCadastro implements Serializable{
+
+public class EnderecoResponse implements Serializable{
 
     private String cep;
     private String logradouro;
@@ -9,6 +10,15 @@ public class ControllerCadastro implements Serializable{
     private String bairro;
     private String localidade;
     private String uf;
+
+    public EnderecoResponse(String cep, String logradouro, String uf, String localidade, String bairro, String complemento) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.uf = uf;
+        this.localidade = localidade;
+        this.bairro = bairro;
+        this.complemento = complemento;
+    }
 
     public String getCep(String cep){
         return cep;
@@ -20,30 +30,32 @@ public class ControllerCadastro implements Serializable{
         return logradouro;
     }
     public void setLogradouro(String logradouro){
-        return logradouro;
+        this.logradouro = logradouro;
     }
     public String getComplemento(String complemento){
         return complemento;
     }
-    public void setComplemento(String complemento){
-        return complemento;
+    public void setComplemento(String complemento)
+    {
+        this.complemento = complemento;
     }
     public String getBairro(String bairro){
         return bairro;
     }
     public void setBairro(String bairro){
-        return bairro;
+        this.bairro = bairro;
     }
     public String getLocalidade(String localidade){
         return localidade;
     }
     public void setLocalidade(String localidade){
-        return localidade;
+        this.localidade = localidade;
     }
     public String getUf(String uf){
         return uf;
     }
     public void setUf(String uf){
-        return uf;
+        this.uf = uf;
     }
+
 }
