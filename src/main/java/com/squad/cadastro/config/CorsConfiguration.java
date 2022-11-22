972917b143de0acb6a3664913184a20c3df39253
Guilherme Nogueira <guilherme.nogueiras@outlook.com>
2022-11-21 21:47:48 -0300

    feat/cors registry configuration

--- src/main/java/com/squad/cadastro/config/CorsConfiguration.java
+++ src/main/java/com/squad/cadastro/config/CorsConfiguration.java
@@ -12,7 +12,10 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://frontend-register-app-squad-cadastro.vercel.app")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://frontend-register-app-squad-cadastro.vercel.app/**",
+                        "https://frontend-register-app-squad-cadastro.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
