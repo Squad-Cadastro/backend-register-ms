3b640bd1e91d8286a6797fe40fa4431ce5e039e9
THONWELLING <thonwellingdani13@gmail.com>
2023-06-18 01:56:54 -0300

    Configurando modelMapper

--- src/main/java/com/squad/cadastro/repository/entity/ClienteEntity.java
+++ src/main/java/com/squad/cadastro/repository/entity/ClienteEntity.java
@@ -1,15 +1,26 @@
 package com.squad.cadastro.repository.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Getter
+@Setter
 @Table(name="cliente_informacoes")
 public class ClienteEntity implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -42,83 +53,4 @@ public class ClienteEntity implements Serializable {
 
     @OneToMany (fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "clienteInformacoes")
     private List<EnderecoEntity> endereco;
-
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) { this.id = id; }
-    public List<EnderecoEntity> getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(List<EnderecoEntity> endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public String getTipoPessoa() {
-        return tipoPessoa;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public LocalDateTime getDataCadastro() {
-        return dataCadastro;
-    }
-
-    public LocalDateTime getDataAtualizacao() {
-        return dataAtualizacao;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-
-    public void setTipoPessoa(String tipoPessoa) {
-        this.tipoPessoa = tipoPessoa;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setDataCadastro(LocalDateTime dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
-
-    public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
-        this.dataAtualizacao = dataAtualizacao;
-    }
-
-
 }
